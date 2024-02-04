9867cac2dc259624bff911344f5aa81a13fbe2e4
CloverClub03 <114269541+CloverClubWork@users.noreply.github.com>
2024-02-04 17:08:31 +0800

    update data/novel-script.js

--- data/novel-script.js
+++ data/novel-script.js
@@ -1,142 +1,163 @@
-$(document).ready(function() {
-  
-  for(let shimmer=0;shimmer < 10; shimmer++){
-    const li = "<li class='shimmer'></li>";
-    $('#topAiring').append(li);
-    $('#seasonal').append(li);
-    $('#latestEpisode').append(li);
-  }
-  
-  fetch('https://anivel.netlify.app/data/routes/anime.json')
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Something went wrong');
-      }
-      return res.json();
-    })
-    .then(data => {
-      const container = $('#latestEpisode');
-      console.log(data)
-      const results = data.data;
-      container.empty();
-      results.forEach(items => {
-        const item = `
-          <li class='item' data-src=''>
-              <div class='item-cover'>
-                <img src='` + items.cover + `' alt='Cover' loading='lazy'/>
-              </div>
-              <p id='item-title'>` + items.title.romaji + `</p>
-          </li>
-        `;
-        container.append(item);
-      });
-      const loadMore = `
-        <li class='viewmore'>
-           <span>View More</span>
-        </li>
-      `;
-      $('#latestEpisode li:last').after(loadMore);
-    })
-    .then(error => {
-      console.log(error);
-    })
-    
-  fetch('https://api.jikan.moe/v4/seasons/now')
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Something went wrong');
-      }
-      return res.json();
-    })
-    .then(data => {
-      const container = $('#seasonal');
-      const results = data.data;
-      container.empty();
-      results.forEach(items => {
-        const item = `
-          <li class='item' data-src=''>
-              <div class='item-cover'>
-                <img src='` + items.images.jpg.large_image_url + `' alt='Cover' loading='lazy'/>
-              </div>
-              <p id='item-title'>` + items.title + `</p>
-          </li>
-        `;
-        container.append(item);
-      });
-      const loadMore = `
-        <li class='viewmore'>
-           <span>View More</span>
-        </li>
-      `;
-      $('#seasonal li:last').after(loadMore);
-    })
-    .then(error => {
-      console.log(error);
-    })
-    
-  fetch('https://api.jikan.moe/v4/top/anime?filter=airing')
-  .then(res => {
-    if(!res.ok){
-      throw new Error('Something went wrong');
+$(document).ready(function () {
+    for (let shimmer = 0; shimmer < 10; shimmer++) {
+        const li = "<li class='shimmer'></li>";
+        $("#topAiring").append(li);
+        $("#seasonal").append(li);
+        $("#latestEpisode").append(li);
     }
-    return res.json();
-  })
-  .then(data => {
-    const container = $('#topAiring');
-    const results = data.data;
-    container.empty();
-    results.forEach(items => {
-      const item = `
+
+    fetch("https://anivel.netlify.app/data/routes/anime.json")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const container = $("#latestEpisode");
+            console.log(data);
+            const results = data.data;
+            container.empty();
+            results.forEach(items => {
+                const item =
+                    `
+          <li class='item' data-src=''>
+              <div class='item-cover'>
+                <img src='` +
+                    items.cover +
+                    `' alt='Cover' loading='lazy'/>
+              </div>
+              <p id='item-title'>` +
+                    items.title.romaji +
+                    `</p>
+          </li>
+        `;
+                container.append(item);
+            });
+            const loadMore = `
+        <li class='viewmore'>
+           <span>View More</span>
+        </li>
+      `;
+            $("#latestEpisode li:last").after(loadMore);
+        })
+        .then(error => {
+            console.log(error);
+        });
+
+    fetch("https://api.jikan.moe/v4/seasons/now")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const container = $("#seasonal");
+            const results = data.data;
+            container.empty();
+            results.forEach(items => {
+                const item =
+                    `
+          <li class='item' data-src=''>
+              <div class='item-cover'>
+                <img src='` +
+                    items.images.jpg.large_image_url +
+                    `' alt='Cover' loading='lazy'/>
+              </div>
+              <p id='item-title'>` +
+                    items.title +
+                    `</p>
+          </li>
+        `;
+                container.append(item);
+            });
+            const loadMore = `
+        <li class='viewmore'>
+           <span>View More</span>
+        </li>
+      `;
+            $("#seasonal li:last").after(loadMore);
+        })
+        .then(error => {
+            console.log(error);
+        });
+
+    fetch("https://api.jikan.moe/v4/top/anime?filter=airing")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const container = $("#topAiring");
+            const results = data.data;
+            container.empty();
+            results.forEach(items => {
+                const item =
+                    `
         <li class='item' data-src=''>
             <div class='item-cover'>
-              <img src='` + items.images.jpg.large_image_url + `' alt='Cover' loading='lazy'/>
+              <img src='` +
+                    items.images.jpg.large_image_url +
+                    `' alt='Cover' loading='lazy'/>
             </div>
-            <p id='item-title'>`+items.title+`</p>
+            <p id='item-title'>` +
+                    items.title +
+                    `</p>
         </li>
       `;
-      container.append(item);
-    })
-    const loadMore = `
+                container.append(item);
+            });
+            const loadMore = `
             <li class='viewmore'>
                <span>View More</span>
             </li>
           `;
-    $('#topAiring li:last').after(loadMore);
-  })
-  .then(error => {
-    console.log(error);
-  })
-  
-  fetch('https://api.jikan.moe/v4/top/anime?filter=bypopularity')
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Something went wrong');
-      }
-      return res.json();
-    })
-    .then(data => {
-      const container = $('#topPopularity');
-      const results = data.data;
-      container.empty();
-      results.forEach(items => {
-        const item = `
+            $("#topAiring li:last").after(loadMore);
+        })
+        .then(error => {
+            console.log(error);
+        });
+
+    fetch("https://api.jikan.moe/v4/top/anime?filter=bypopularity")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const container = $("#topPopularity");
+            const results = data.data;
+            container.empty();
+            results.forEach(items => {
+                const item =
+                    `
           <li class='item' data-src=''>
             <div>
-              <img src='`+items.images.jpg.image_url+`' alt='Cover' loading='lazy'/>
+              <img src='` +
+                    items.images.jpg.image_url +
+                    `' alt='Cover' loading='lazy'/>
             <div class='info'>
-              <p>`+items.title+`</p>
-              <p>`+items.type+`</p>
-              <p>`+items.rating+`</p>
+              <p>` +
+                    items.title +
+                    `</p>
+              <p>` +
+                    items.type +
+                    `</p>
+              <p>` +
+                    items.rating +
+                    `</p>
             </div>
             </div>
           </li>
         `;
-        container.append(item);
-      })
-      
-    })
-    .then(error => {
-      console.log(error);
-    })
-
+                container.append(item);
+            });
+        })
+        .then(error => {
+            console.log(error);
+        });
 });
