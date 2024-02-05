fa30569c8c56544daeb08e5ae9910a93b1c77e7e
CloverClub03 <114269541+CloverClubWork@users.noreply.github.com>
2024-02-05 12:10:58 +0800

    update data/datainfo.js

--- data/datainfo.js
+++ data/datainfo.js
@@ -15,7 +15,8 @@ $(document).ready(function () {
     }
     const idParam = getQueryParam("id");
     let params = "https://api.jikan.moe/v4/anime/" + idParam + "/full";
-    let episodeParam = 'https://consumetmyapi.vercel.app/meta/mal/info/'+idParam;
+    let episodeParam =
+        "https://consumetmyapi.vercel.app/meta/mal/info/" + idParam;
     fetch(params)
         .then(response => {
             if (!response.ok) {
@@ -37,78 +38,143 @@ $(document).ready(function () {
             } else {
                 status = '<i class="fa-solid fa-clock"></i>';
             }
-            $("#status").html(status+ ' '+results.status);
-            $("#type").html('<i class="fa-solid fa-tv"></i> ' +results.type);
-            $("#source").html('<i class="fa-solid fa-book"></i> ' +results.source);
-            $("#duration").html('<i class="fa-solid fa-hourglass"></i> ' +results.duration);
+            $("#status").html(status + " " + results.status);
+            $("#type").html('<i class="fa-solid fa-tv"></i> ' + results.type);
+            $("#source").html(
+                '<i class="fa-solid fa-book"></i> ' + results.source
+            );
+            $("#duration").html(
+                '<i class="fa-solid fa-hourglass"></i> ' + results.duration
+            );
             $("#overview").html(results.synopsis);
             let aired;
-            if(results.aired.prop.to.year == null){
-              aired = results.aired.prop.from.year+'-'+results.aired.prop.from.month+'-'+results.aired.prop.from.day+' to ?';
-            }else{
-              aired = results.aired.prop.from.year+'-'+results.aired.prop.from.month+'-'+results.aired.prop.from.day+' to '+results.aired.prop.to.year+'-'+results.aired.prop.to.month+'-'+results.aired.prop.to.day;
+            if (results.aired.prop.to.year == null) {
+                aired =
+                    results.aired.prop.from.year +
+                    "-" +
+                    results.aired.prop.from.month +
+                    "-" +
+                    results.aired.prop.from.day +
+                    " to ?";
+            } else {
+                aired =
+                    results.aired.prop.from.year +
+                    "-" +
+                    results.aired.prop.from.month +
+                    "-" +
+                    results.aired.prop.from.day +
+                    " to " +
+                    results.aired.prop.to.year +
+                    "-" +
+                    results.aired.prop.to.month +
+                    "-" +
+                    results.aired.prop.to.day;
             }
             $("#airedTime").text(aired);
             const premiered = results.season;
-            $("#season").text(premiered.toUpperCase()+' '+results.year);
+            $("#season").text(premiered.toUpperCase() + " " + results.year);
             const genres = results.genres;
             genres.forEach(genres => {
-              const item =
-              `
+                const item =
+                    `
                 <li>
-                  <p>`+genres.name+`</p>
+                  <p>` +
+                    genres.name +
+                    `</p>
                 </li>
               `;
-              $('#genres').append(item);
+                $("#genres").append(item);
             });
             const studios = results.studios;
             studios.forEach(studios => {
-              const item =
-              `
+                const item =
+                    `
                 <li>
-                  <p>`+studios.name+`</p>
+                  <p>` +
+                    studios.name +
+                    `</p>
                 </li>
               `;
-              $('#studios').append(item);
+                $("#studios").append(item);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    //Episodes
+    fetch(episodeParam)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const results = data.episodes;
+            results.forEach(episodes => {
+                let epTitle, epSynopsis, epCover;
+                epCover = episodes.image;
+                if (episodes.title == null || episodes.title == undefined) {
+                    epTitle = "Episode " + episodes.number;
+                }else{
+                  epTitle = episodes.title;
+                }
+                if (episodes.description == null) {
+                    epSynopsis = "Stream Episode " + episodes.number + " on HD";
+                }
+                const episode =
+                    `
+        <li data-src=''>
+          <img src='` +
+                    epCover +
+                    `' alt='Episode Cover' loading='lazy'/>
+          <div>
+            <h5>` +
+                    epTitle +
+                    `</h5>
+            <p>` +
+                    epSynopsis +
+                    `</p>
+          </div>
+        </li>
+        `;
+                $("#episodes").append(episode);
             });
         })
         .catch(error => {
             console.log(error);
         });
         
-    //Episodes
-    fetch(episodeParam)
-    .then(response => {
-      if(!response.ok){
-        throw new Error('Something went wrong!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const results = data.episodes;
-      results.forEach(episodes => {
-        let epTitle, epSynopsis, epCover;
-        epCover = episodes.image;
-        if(episodes.title == null){
-          epTitle = 'Episode '+episodes.number;
-        }
-        if(episodes.description == null){
-          epSynopsis = 'Stream Episode '+episodes.number+' on HD';
-        }
-        const episode =
-        `
-        <li data-src=''>
-          <img src='`+epCover+`' alt='Episode Cover' loading='lazy'/>
-          <div>
-            <h5>`+epTitle+`</h5>
-            <p>`+epSynopsis+`</p>
-          </div>
-        </li>
-        `;
-        $('#episodes').append(episode);
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    const list = $("#episodes");
+                const itemsPerPage = 3; // Set the number of items per page
+                const totalItems = list.children("li").length;
+                console.log(totalItems)
+                let currentPage = 1;
+
+                function showItems(page) {
+                    const startIndex = (page - 1) * itemsPerPage;
+                    const endIndex = startIndex + itemsPerPage;
+
+                    list.find("li").hide().slice(startIndex, endIndex).show();
+                }
+
+                function renderPagination() {
+                    const totalPages = Math.ceil(totalItems / itemsPerPage);
+                    const paginationContainer = $("#pagination");
+                    paginationContainer.empty();
+
+                    for (let i = 1; i <= totalPages; i++) {
+                        const pageLink = $(
+                            '<span class="page-link">' + i + "</span>"
+                        );
+                        pageLink.click(function () {
+                            currentPage = i;
+                            showItems(currentPage);
+                        });
+                        paginationContainer.append(pageLink);
+                    }
+                }
+                showItems(currentPage);
+                renderPagination();
 });
