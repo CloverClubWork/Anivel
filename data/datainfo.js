8481d53f78a94f0b7a745ea36afedfa8bbbe97af
CloverClub03 <114269541+CloverClubWork@users.noreply.github.com>
2024-02-05 09:25:02 +0800

    update data/datainfo.js

--- data/datainfo.js
+++ data/datainfo.js
@@ -1,26 +1,38 @@
 $(document).ready(function () {
+    function formatNumber(number) {
+        if (number >= 1000) {
+            // Divide the number by 1000 and append 'k'
+            return (number / 1000).toFixed(1) + "k";
+        } else {
+            // If the number is less than 1000, just return it as is
+            return number.toString();
+        }
+    }
+
     function getQueryParam(key) {
         const urlParams = new URLSearchParams(window.location.search);
         return urlParams.get(key);
     }
     const idParam = getQueryParam("id");
-    let params = 'https://api.jikan.moe/v4/anime/'+idParam+'/full';
-    
+    let params = "https://api.jikan.moe/v4/anime/" + idParam + "/full";
+
     fetch(params)
-    .then(response => {
-      if(!response.ok){
-        throw new Error('Something went wrong!');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const results = data.data;
-      $('#animeCover').attr('src', results.images.webp.large_image_url);
-      $('#anime-title').text(results.title_english);
-      $('#anime-title-romaji').text(results.title);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-   
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const results = data.data;
+            $("#animeCover").attr("src", results.images.webp.large_image_url);
+            $("#anime-title").text(results.title_english);
+            $("#anime-title-romaji").text(results.title);
+            $("#score").text(results.score);
+            const favorites = formatNumber(results.favorites);
+            $("#favorites").text(favorites);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
